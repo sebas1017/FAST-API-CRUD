0edf2b956e76dfcc1e31e7adb6cbee0b7ce156a1
sebas1017 <henaoerazosebastian995@gmail.com>
2022-04-20 16:54:04 -0500

    ajuste de variables de entorno

--- database.py
+++ database.py
@@ -1,4 +1,15 @@
+import os
 from peewee import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database = os.getenv('DATABASE')
+user = os.getenv('USER')
+host = os.getenv('HOST')
+port = os.getenv('PORT')
+password = os.getenv('PASSWORD')
+
 
 database = PostgresqlDatabase(
     database="d6349dtkdlvoau",
