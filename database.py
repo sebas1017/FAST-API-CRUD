1b90f640c6b0dafcc6ccbb3ebed65178738f6850
sebas1017 <henaoerazosebastian995@gmail.com>
2022-04-23 08:21:42 -0500

    ajustes en credenciales

--- database.py
+++ database.py
@@ -12,8 +12,8 @@ password = os.getenv('PASSWORD')
 
 
 database = PostgresqlDatabase(
-    database="d6349dtkdlvoau",
-    user="qsivylwlywidad", password="6c83829dd59dae2c383f416483e2efe40c086bd2d3f2cbd2869bf99fa463a5e1",
-    host="ec2-63-32-248-14.eu-west-1.compute.amazonaws.com", port=5432
+    database=database,
+    user=user, password=password,
+    host=host, port=5432
 
 )
